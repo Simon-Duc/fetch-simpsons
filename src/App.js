db503d715ffcfc6f972a85524dbe3924f7f9f1ea
Simon Duc <ducsimon@gmail.com>
2022-04-11 12:20:49 +0200

    import axios, consumed the simpsons api, finalized the exercise

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import './App.css';
+import QuoteCard from './components/QuoteCard';
+import React from 'react';
+import axios from 'axios';
+
+const initialQuote = 
+  {
+    quote:
+      "These are my only friends...grown-up nerds like Gore Vidal. And even he's kissed more boys than I ever will.",
+    character: 'Lisa Simpson',
+    image:
+      'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083',
+  };
 
 function App() {
+  const [newQuote, setNewQuote] = React.useState(initialQuote);
+
+  const getQuote = () => {
+       // Send the request
+       axios
+       .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+       // Extract the DATA from the received response
+       .then((response) => response.data)
+       // Use this data to update the state
+       .then((data) => {
+         console.log(data)
+         setNewQuote(data[0]);
+       });
+  };
+
   return (
     <div>
-
+      <button type="button" onClick={getQuote}>
+        Give me another quote
+      </button>
+      <QuoteCard quote={newQuote}/>
     </div>
   );
 }
---
+++ src/components/QuoteCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './QuoteCard.css';
+
+const QuoteCard = ({ quote }) => (
+  quote && (
+  <figure className="QuoteCard">
+    <img src={quote.image} alt={quote.character} />
+    <figcaption>
+      <blockquote>{quote.quote}</blockquote>
+      <cite>{quote.character}</cite>
+    </figcaption>
+  </figure>)
+);
+
+export default QuoteCard;
